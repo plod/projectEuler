25b1bdab4cc3c03300f66019153f409a0b147949
Daniel T. Morgan <dtmorgan@fields-group.com>
2016-03-23 08:54:12 +0000

    number 16

---
+++ first100/016/main.go
@@ -0,0 +1,38 @@
+/*
+Projecteuler problem 1t https://projecteuler.net/problem=1t
+
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+package main
+
+import (
+	"log"
+	"math/big"
+	"strconv"
+)
+
+const powerOfTwo = 1000
+
+func findPower(i int64) *big.Int {
+	returnVal := new(big.Int).Exp(big.NewInt(2), big.NewInt(i), nil)
+	log.Printf("Power Return Val = %d\n", returnVal)
+	return returnVal
+}
+
+func sumOfDigits(number *big.Int) int {
+	stringOfNumber := number.String()
+	var sum int
+	for i := 0; i < len(stringOfNumber); i++ {
+		var digit int
+		digit, _ = strconv.Atoi(string(stringOfNumber[i]))
+		log.Println(digit)
+		sum += digit
+	}
+	return sum
+}
+
+func main() {
+	log.Println(sumOfDigits(findPower(powerOfTwo)))
+}
