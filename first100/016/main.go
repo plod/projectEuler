ba9aac8c6be90b9a0351b6e2a7c9cb9f54b22d1a
Daniel T. Morgan <dtmorgan@fields-group.com>
2016-03-24 09:10:15 +0000

    my first attempt at vendoring

--- first100/016/main.go
+++ first100/016/main.go
@@ -1,5 +1,5 @@
 /*
-Projecteuler problem 1t https://projecteuler.net/problem=1t
+Projecteuler problem 16 https://projecteuler.net/problem=16
 
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
